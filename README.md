35cc44469c47392a6340c5f764535d26445b3bc1
mzsm <mizushimaj@gmail.com>
2018-05-26 21:26:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 日大アメフト部加害選手に対する減刑嘆願書作成ツール
+
+## ライセンス
+
+Apache License 2.0
+
+[http://www.apache.org/licenses/LICENSE-2.0]
---
+++ js/petition.js
@@ -0,0 +1,41 @@
+let signature = new fabric.Canvas('signature');
+signature.isDrawingMode = true;
+
+let output = new fabric.Canvas('output');
+output.selection = false;
+
+// リセット
+document.querySelector('#reset').addEventListener('click', function() {
+    signature.clear();
+}, false);
+
+// 画像生成
+document.querySelector('#form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    output.clear();
+    output.setBackgroundImage('img/template.jpg', output.renderAll.bind(output));
+
+    let address = document.querySelector('#address').value;
+    let addressText = new fabric.Text(
+        address,
+        {
+            left: 160,
+            top: 510,
+            fontSize: 16,
+            fontFamily: '"游明朝",YuMincho,"ヒラギノ明朝 ProN W3","Hiragino Mincho ProN","ＭＳ明朝",serif'
+        }
+    );
+    output.add(addressText);
+
+    let signaturePNG = signature.toDataURL('image/png');
+    fabric.Image.fromURL(signaturePNG, function (img) {
+        img.top = 630;
+        img.left = 120;
+        output.add(img);
+    });
+
+    document.querySelector('#outputContainer').style.display = 'block';
+    return false;
+}, false);
+
