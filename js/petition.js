475585233d0304c01d7e29200949bceb8396fbed
mzsm <mizushimaj@gmail.com>
2018-05-26 22:04:57 +0900

    保存ボタン

--- js/petition.js
+++ js/petition.js
@@ -1,8 +1,5 @@
-let signature = new fabric.Canvas('signature');
-signature.isDrawingMode = true;
-
-let output = new fabric.Canvas('output');
-output.selection = false;
+let signature = new fabric.Canvas('signature',{isDrawingMode: true});
+let output = new fabric.StaticCanvas('output', {interactive: false, selection: false, selectable: false});
 
 // リセット
 document.querySelector('#undo').addEventListener('click', function() {
@@ -21,26 +18,30 @@ document.querySelector('#form').addEventListener('submit', function(e) {
     e.preventDefault();
 
     output.clear();
-    output.setBackgroundImage('img/template.jpg', output.renderAll.bind(output));
+    fabric.Image.fromURL('img/template.jpg', function(img){
+        output.setBackgroundImage(img, output.renderAll.bind(output));
 
-    let address = document.querySelector('#address').value;
-    let addressText = new fabric.Text(
-        address,
-        {
-            left: 160,
-            top: 510,
-            fontSize: 16,
-            fontFamily: '"游明朝",YuMincho,"ヒラギノ明朝 ProN W3","Hiragino Mincho ProN","ＭＳ明朝",serif'
-        }
-    );
-    output.add(addressText);
+        let address = document.querySelector('#address').value;
+        let addressText = new fabric.Text(
+            address,
+            {
+                left: 160,
+                top: 510,
+                fontSize: 16,
+                fontFamily: '"游明朝",YuMincho,"ヒラギノ明朝 ProN W3","Hiragino Mincho ProN","ＭＳ明朝",serif'
+            }
+        );
+        output.add(addressText);
 
-    let signaturePNG = signature.toDataURL('image/png');
-    fabric.Image.fromURL(signaturePNG, function (img) {
-        img.top = 630;
-        img.left = 100;
-        img.scale(0.5);
-        output.add(img);
+        let signaturePNG = signature.toDataURL('image/png');
+        fabric.Image.fromURL(signaturePNG, function (img) {
+            img.top = 630;
+            img.left = 100;
+            img.scale(0.5);
+            output.add(img);
+
+            document.querySelector('#save').href = output.toDataURL('image/png');
+        });
     });
 
     document.querySelector('#outputContainer').style.display = 'block';
